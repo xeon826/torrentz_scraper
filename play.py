e8ae3f518e3824d87ce090eba682126ad75e8f2e
Joshua W <xeon826@gmail.com>
2023-06-13 12:22:30 -0500

    parse selection

--- play.py
+++ play.py
@@ -1,10 +1,47 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
+
+# query = "the professional"
 
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://torrentz2.nz/search?q=the+ritual']
+class MySpider(scrapy.Spider):
+    query = input("Query: ")
+    custom_settings = {"LOG_ENABLED": False}
+    name = "blogspider"
+    results = []
+    # start_urls = ['https://torrentz2.nz/search?q=the+ritual']
+    start_urls = ["https://torrentz2.nz/search?q=%s" % str(query)]
 
     def parse(self, response):
-        for href in response.css("dd > span > a::attr('href')").extract():
-            print(href)
+        # for href in response.css("dd > span > a::attr('href')").extract():
+        for i, row in enumerate(response.css(".results > dl")):
+            title = row.css("dt > a::text").extract()[0]
+            magnet = row.css("dd > span > a::attr('href')").extract()
+            seeds = row.css("dd > span:nth-child(4)::text").extract()[0]
+            leeches = row.css("dd > span:nth-child(5)::text").extract()[0]
+            self.results.append(
+                {"title": title, "magnet": magnet, "seeds": seeds, "leeches": leeches}
+            )
+            if i >= 20:
+                break
+        for i, obj in enumerate(self.results):
+            print("%s) %s %s %s" % (str(i), obj["title"], obj["seeds"], obj["leeches"]))
+        selection = input('0-20 to select, r to perform new query, x to quit: ')
+        if selection == 'r':
+            self.query = input('Query: ')
+            process.crawl(MySpider)
+            process.start()
+        elif selection == 'x':
+            print('bye')
+        else:
+            print("selected %s" % self.results[int(selection)]['title'])
+
+
+process = CrawlerProcess(
+    {
+        "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.15.3 Chrome/87.0.4280.144 Safari/537.36"
+    }
+)
+
+process.crawl(MySpider)
+process.start()
--- torrent.py
+++ torrent.py
@@ -1,13 +1,15 @@
 from qbittorrent import Client
 import time
 
-qb = Client("http://127.0.0.1:8080/")
-qb.login('admin', 'adminadmin')
-qb.download_from_link('magnet:?xt=urn:btih:2117A0A5C7A53A25163152C77EAB431C7C1D9F4E&tr=udp%3A%2F%2Ftracker.bitsearch.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker2.dler.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.breizh.pm%3A6969%2Fannounce&tr=udp%3A%2F%2Fwww.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&dn=%5BBitsearch.to%5D+The.Ritual.Killer.2023.1080p.WEBRip.x265-RARBG')
+class Torrent():
+    qb = Client("http://127.0.0.1:8080/")
+    qb.login('admin', 'adminadmin')
+    qb.download_from_link()
 
-
-while True:
-    print(qb.torrents()[1])
-    time.sleep(1)
+    download(magnet):
+        qb.download_from_link(magnet)
+        while True:
+            print(qb.torrents()[1])
+            time.sleep(1)
 
 
