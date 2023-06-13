7abc6744dea04e5632a0996cab4be5d31683274b
Joshua W <xeon826@gmail.com>
2023-06-13 15:05:44 -0500

    interrupted method

--- play.py
+++ play.py
@@ -6,14 +6,13 @@ from scrapy.crawler import CrawlerProcess
 
 class MySpider(scrapy.Spider):
     query = input("Query: ")
-    custom_settings = {"LOG_ENABLED": False}
+    # custom_settings = {"LOG_ENABLED": False}
     name = "blogspider"
     results = []
     # start_urls = ['https://torrentz2.nz/search?q=the+ritual']
     start_urls = ["https://torrentz2.nz/search?q=%s" % str(query)]
 
     def parse(self, response):
-        # for href in response.css("dd > span > a::attr('href')").extract():
         for i, row in enumerate(response.css(".results > dl")):
             title = row.css("dt > a::text").extract()[0]
             magnet = row.css("dd > span > a::attr('href')").extract()
@@ -24,24 +23,9 @@ class MySpider(scrapy.Spider):
             )
             if i >= 20:
                 break
-        for i, obj in enumerate(self.results):
-            print("%s) %s %s %s" % (str(i), obj["title"], obj["seeds"], obj["leeches"]))
-        selection = input('0-20 to select, r to perform new query, x to quit: ')
-        if selection == 'r':
-            self.query = input('Query: ')
-            process.crawl(MySpider)
-            process.start()
-        elif selection == 'x':
-            print('bye')
-        else:
-            print("selected %s" % self.results[int(selection)]['title'])
+
+    def get_results(self):
+        return self.results
 
 
-process = CrawlerProcess(
-    {
-        "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.15.3 Chrome/87.0.4280.144 Safari/537.36"
-    }
-)
 
-process.crawl(MySpider)
-process.start()
--- torrent.py
+++ torrent.py
@@ -1,15 +1,48 @@
-from qbittorrent import Client
 import time
 
-class Torrent():
+from clint.textui import progress
+from qbittorrent import Client
+
+
+class Torrent:
     qb = Client("http://127.0.0.1:8080/")
-    qb.login('admin', 'adminadmin')
-    qb.download_from_link()
+    qb.login("admin", "adminadmin")
+    torrent_index = ''
+    stop = False
+    # qb.download_from_link()
 
-    download(magnet):
-        qb.download_from_link(magnet)
-        while True:
-            print(qb.torrents()[1])
-            time.sleep(1)
+    def download(self, magnet):
+        self.qb.download_from_link(magnet)
+        # print(self.qb.torrents()[-1])
+        torrents = self.qb.torrents()
+        active_torrent_index = ""
+        for i, torrent in enumerate(self.qb.torrents()):
+            if self.get_hash(torrent["magnet_uri"]) == self.get_hash(magnet):
+                self.torrent_index = i
+                break
 
+    def stop(self):
+        self.stop = True
 
+    def show_progress(self):
+        print(
+            "%s %",
+            str(
+                (
+                    self.qb.torrents()[self.torrent_index]["completed"]
+                    / self.qb.torrents()[self.torrent_index]["total_size"]
+                )
+                * 100
+            ),
+        )
+
+    def get_hash(self, magnet):
+        return magnet.split("&")[0].upper()
+
+        # for torrent in self.qb.torrents():
+        #     if torrent['magnet_uri'] == magnet:
+        #         print('IF')
+        #         while torrent['amount_left'] != 0:
+        #             print('WHILE')
+        #             print((torrent['completed'] / torrent['completion_on']) * 100 + '% done')
+        #             time.sleep(1)
