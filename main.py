357ba0c19b43df4f7c9617c86184cbcf60379394
Joshua W <xeon826@gmail.com>
2023-07-16 23:50:33 -0500

    to find moov atom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+
+# from pymp4.parser import Box, BoxReader, MovieHeaderBox
 import subprocess
 import sys
 from threading import Event
@@ -71,9 +73,9 @@ def main():
             clear = lambda: os.system("clear")
             clear()
             if torrent.get_raw_progress() > 40 and not playing:
-                # TODO use qtfaststart to determine if file has moov atom
                 subprocess.run(
-                    'nohup mpv "%s" --profile=low-latency --log-file=output.txt &'
+                    # 'nohup mpv "%s" --profile=low-latency --log-file=output.txt &'
+                    'nohup mpv "%s" --log-file=output.txt &'
                     % torrent.active_torrent["content_path"],
                     shell=True,
                 )
@@ -91,6 +93,29 @@ def main():
                 torrent.stop()
                 break
     process.stop()
+
+
+# def has_moov_atom(file_path):
+#     with open(file_path, 'rb') as f: #todo pass file path here
+#         reader = BoxReader(f)
+#         root = reader.read()
+#     moov_box = None
+
+#     for box in root.children:
+#         if box.type == 'moov':
+#             moov_box = box
+#             break
+
+#     if moov_box:
+#         # Perform validation on the moov atom
+#         # You can access and check its child boxes, metadata, and structure
+#         # For example, you can check if it contains necessary boxes like 'mvhd' (MovieHeaderBox)
+#         if any(isinstance(child, MovieHeaderBox) for child in moov_box.children):
+#             return True # moov atom present and valid
+#         else:
+#             return False # moov atom present but not valid
+#     else:
+#         return False # moov atom not present
 
 
 def quit(signo, _frame):
